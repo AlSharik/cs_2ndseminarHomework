d1af8d8738e29a927f2e00c2dc88cf0e58a18d8c
Alexander Shar <alahan05@gmail.com>
2022-10-05 17:32:27 +0300

    Second way of solving

--- task4/Program.cs
+++ task4/Program.cs
@@ -4,6 +4,7 @@
 
 // Это первый вариант решения если дни недели могут быть только 1-7
 
+/*
 System.Console.WriteLine("Write number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
@@ -22,4 +23,23 @@ else
 {
     System.Console.WriteLine("wrong input");
 }
+*/
+// Второй вариант решение, если дни недели могут любыми целыми положительными числами
+System.Console.WriteLine("Write number: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
+if(number >= 1)
+{
+    if(number % 7 == 0 || number % 7 == 6)
+    {
+        System.Console.WriteLine("Yeah, its weekend");
+    }
+    else
+    {
+        System.Console.WriteLine("Sorry, but you should go to work");
+    }
+}
+else
+{
+    System.Console.WriteLine("wrong input");
+}
