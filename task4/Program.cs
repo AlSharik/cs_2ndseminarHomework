42d89d5e03a83ae55fca529dbbb6470220d6fcaf
Alexander Shar <alahan05@gmail.com>
2022-10-05 17:29:31 +0300

    First way of solving

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+
+// Это первый вариант решения если дни недели могут быть только 1-7
+
+System.Console.WriteLine("Write number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number <= 7 && number >= 1)
+{
+    if(number == 7 || number == 6)
+    {
+        System.Console.WriteLine("Yeah, its weekend");
+    }
+    else
+    {
+        System.Console.WriteLine("Sorry, but you should go to work");
+    }
+}
+else
+{
+    System.Console.WriteLine("wrong input");
+}
+
