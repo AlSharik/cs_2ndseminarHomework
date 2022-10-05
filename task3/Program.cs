9a5a40cb405445ce9b489bc41cd7a03520a3d1d4
Alexander Shar <alahan05@gmail.com>
2022-10-05 17:19:43 +0300

    3rd task ended

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 3: Напишите программу, которая выводит третью цифру заданного 
+числа или сообщает, что третьей цифры нет.
+Не использовать строки для расчета.
+*/
+
+Console.WriteLine("Write number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 100)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while(number > 999)
+    {
+        number /= 10;
+    }
+    System.Console.WriteLine(number % 10);
+}
