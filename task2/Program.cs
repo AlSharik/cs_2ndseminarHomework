72c98739c6762ee0874afd952913d3e0a0ae309b
Alexander Shar <alahan05@gmail.com>
2022-10-05 17:11:50 +0300

    2nd task ended

---
+++ task2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и
+// удаляет вторую цифру этого числа.
+// Не использовать строки для расчета.
+
+int random = new Random().Next(100, 1000);
+System.Console.WriteLine(random);
+
+System.Console.WriteLine($"{random/100}{random%10}");
