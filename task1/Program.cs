eed860afe4f11b209c30b3f18f91decd8d3b6516
Alexander Shar <alahan05@gmail.com>
2022-10-05 17:06:50 +0300

    1 task ended

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 1: Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа. 
+Не использовать строки для расчета
+*/
+Console.WriteLine("Write number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Проверка на трехназначность не нужна так как в условиях сказанно, что число на входе будет 3-ех значное
+// но я ее на всякий напишу
+
+if( number > 999 || number < 100)
+{
+    System.Console.WriteLine("Wrong number");
+}
+else
+{
+    System.Console.WriteLine($"2nd num is {(number/10)%10}");
+}
